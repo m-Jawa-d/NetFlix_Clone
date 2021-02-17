85ea4bdb40f3ee405ac3a3fabed573f91bcce04e
MuhammadJawad <chudhryjawad@gmail.com>
2021-02-17 21:23:51 +0500

    NetflixClone

---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <>
+            <div className="footer_p">
+                <p>Created by Muhamad Jawad||All Right Reserved ©️ 2021</p>
+            </div>
+        </>
+    )
+}
+
+export default Footer
